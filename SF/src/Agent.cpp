2e7741b4a6eeea2d79d058439598a6b93da908d6
Dmitry Rybokonenko <dmitry.rybokonenko@gmail.com>
2015-08-14 17:55:48 +0300

    deleted redundant construction

--- SF/src/Agent.cpp
+++ SF/src/Agent.cpp
@@ -315,13 +315,8 @@ namespace SF
 	if(fabs(radianX) > 0.001f)
 	{
 		omega = Vector3((getRoll(sim_->globalTime_ + sim_->timeStep_ / 2, radianX).x() - getRoll(sim_->globalTime_ + sim_->timeStep_ / 2, radianX).x()) / sim_->timeStep_, 0, 0);
-		dOmega = Vector3(
-			(getOmega(sim_->globalTime_ + sim_->timeStep_ / 2, sim_->timeStep_, radianX).x() - getOmega(sim_->globalTime_ - sim_->timeStep_ / 2, sim_->timeStep_, radianX).x()) / sim_->timeStep_,
-			0,
-			0);
+		dOmega = Vector3((getOmega(sim_->globalTime_ + sim_->timeStep_ / 2, sim_->timeStep_, radianX).x() - getOmega(sim_->globalTime_ - sim_->timeStep_ / 2, sim_->timeStep_, radianX).x()) / sim_->timeStep_,	0, 0);
 		
-		//dOmega = getDOmega(sim_->globalTime_, sim_->timeStep_, radianX);
-	
 		R = Vector3(position_.x(), position_.y(), 0);
 		V = Vector3(velocity_.x(), velocity_.y(), 0);
 	
@@ -337,10 +332,7 @@ namespace SF
 
 		fixedA = getCross(omega, getCross(omega, fixedR)) + getCross(dOmega, fixedR) - 2 * getCross(omega, fixedV);
 	
-		A = Vector3(
-			fixedA.x() / cos(omega.x()),
-			fixedA.y() / cos(omega.y()),
-			0);
+		A = Vector3(fixedA.x() / cos(omega.x()), fixedA.y() / cos(omega.y()), 0);
 
 		newVX = Vector2(A.x(), A.y());
 	}
