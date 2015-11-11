f689d0e0494cae29ce6464a69e820ff21e84dafa
Dmitry Rybokonenko <dmitry.rybokonenko@gmail.com>
2015-11-11 17:30:09 +0300

    deleted old cut-like method of vector sum

--- SF/src/Agent.cpp
+++ SF/src/Agent.cpp
@@ -239,13 +239,6 @@ namespace SF
 			}
 		}
 
-		// old force sum section
-		/* 
-		auto forceSumLength = getLength(forceSum);
-		sum = forceSum;
-		*/
-
-		// weight section
 		auto size = forces.size();
 		float lengthSum = 0;
 		for (size_t i = 0; i < size; i++)
@@ -259,17 +252,7 @@ namespace SF
 		auto total = Vector2();
 		for (size_t i = 0; i < size; i++)
 			total += forces[i] * forceWeightList[i];
-		//
-
-		/*if (forceSumLength > maxForceLength)
-		{
-			auto coeff = maxForceLength / forceSumLength;
-			forceSum *= coeff;
-			obstaclePressure_ = forceSumLength * coeff;
-		}
-		else
-			obstaclePressure_ = forceSumLength;*/
-
+		
 		obstaclePressure_ = getLength(total);
 		correction += total;
 		obstacleTrajectory_ = position_ + total * 10;
